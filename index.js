962d429912ee9d1cbafa300108e40bd34707aae7
Kyle Crews <kyledcrews@gmail.com>
2019-03-08 21:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === lowercase)
-    return "I can\'t hear you!"
+  if (string.toLowerCase === string) {
+    return "I can\'t hear you!";
+  }
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!';
+  }
+  if (string === "I love you, Grandma.") {
+    return 'I love you, too';
+  }
 }
